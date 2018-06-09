27777aab9ba845a1a0da3d00dd9bc77ecdf107ca
Jascha Scholz <jascha_scholz@web.de>
2018-06-09 17:18:58 +0200

    Updated the java doc of ICellPosition.

--- src/main/java/de/jscholz/jminesweeper/minesweeper/ICellPosition.java
+++ src/main/java/de/jscholz/jminesweeper/minesweeper/ICellPosition.java
@@ -1,20 +1,29 @@
 package de.jscholz.jminesweeper.minesweeper;
 
 /**
- * A interface of a cell position.
+ * <p>
+ *     The interface <i>ICellPosition</i> represents the cell positions inside the minefield. It is used by the cells
+ *     and shows where a cell is located inside the minefield.
+ * </p>
  */
 public interface ICellPosition {
     /**
-     * Returns the x position of the cell.
-     * @return x component of the cell.
+     * Returns the x value of the position.
+     * @return The value of x.
      */
     int getX();
 
     /**
-     * Returns the y position of the cell.
-     * @return y component of the cell.
+     * Returns the y value of the position.
+     * @return The value of y.
      */
     int getY();
 
+    /**
+     * Compares the cell position with the given x and y values.
+     * @param x The x value will be compared to the x value of the cell position.
+     * @param y The y value will be compared to the y value of the cell position.
+     * @return True, if the position is equal to the given values. False, otherwise.
+     */
     boolean compare(final int x, final int y);
 }
